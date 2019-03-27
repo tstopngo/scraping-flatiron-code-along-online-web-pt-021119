986ed0d4d97ef2308844de5efd78bc20a033800f
Tracy Ngo <tngo1206@gmail.com>
2019-03-27 03:16:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,8 +34,9 @@ class Scraper
   
   def make_courses
     get_courses.each do |course| 
-      course = Course.new
-      cours.
+      course.title = course.css("h2").text
+      course.schedule = course.css(".date").text
+      course.description = course.css("p").text
   
 end
 
