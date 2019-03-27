dc49bec24b22af265ef86fb4b6772d798ddbc978
Tracy Ngo <tngo1206@gmail.com>
2019-03-27 03:21:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
     self.make_courses
     Course.all.each do |course|
       if course.title
-        puts "Title: #{course.title}"
+        puts "  Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
       end
